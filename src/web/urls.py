02ca6b0bf97043ae6fd83c1ff834e2e3abedc5ee
arcstur <arcstur@gmail.com>
2024-06-13 19:18:30 -0300

    feat: implement views for saving a development access token

--- src/web/urls.py
+++ src/web/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 
 from .views import (
     login,
+    login_dev,
+    profile,
 )
 
 urlpatterns = [
-    path("auth/login/", login),
+    path("auth/login/", login, name="login"),
+    path("auth/login/dev/", login_dev, name="login_dev"),
+    path("auth/profile/", profile, name="profile"),
 ]
--- src/web/views.py
+++ src/web/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
 
 
 def login(request):
     return render(request, "login.html", {})
+
+
+def login_dev(request):
+    if request.method == "POST":
+        # obtain dev token
+        token = request.POST["access_token"]
+
+        # save access token in the user's session
+        request.session["access_token"] = token
+
+        return redirect("/auth/profile/")
+    else:
+        return render(request, "login_dev.html", {})
+
+
+def profile(request):
+    data = {
+        "access_token": request.session.get("access_token"),
+    }
+    return render(request, "profile.html", data)
