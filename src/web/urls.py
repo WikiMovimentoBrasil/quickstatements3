d48a5014bf5f8ae06facdf73fe09f51913549766
arcstur <arcstur@gmail.com>
2024-06-13 19:27:45 -0300

    feat: show username in profile view
    * add requests as dependency
    * create logout endpoint

--- src/web/urls.py
+++ src/web/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 
 from .views import (
     login,
+    logout,
     login_dev,
     profile,
 )
 
 urlpatterns = [
     path("auth/login/", login, name="login"),
+    path("auth/logout/", logout, name="logout"),
     path("auth/login/dev/", login_dev, name="login_dev"),
     path("auth/profile/", profile, name="profile"),
 ]
--- src/web/views.py
+++ src/web/views.py
@@ -1,8 +1,15 @@
+import requests
+
 from django.shortcuts import render, redirect
 
 
 def login(request):
     return render(request, "login.html", {})
+
+
+def logout(request):
+    request.session.flush()
+    return redirect("/")
 
 
 def login_dev(request):
@@ -19,7 +26,22 @@ def login_dev(request):
 
 
 def profile(request):
+    # TODO: move this logic into the api app
+    access_token = request.session.get("access_token")
+    if access_token is not None:
+        headers = {
+            "Authorization": f"Bearer {access_token}",
+            "Content-Type": "application/json",
+        }
+        response = requests.get(
+            "https://www.mediawiki.org/w/rest.php/oauth2/resource/profile",
+            headers=headers,
+        )
+        username = response.json()["username"]
+    else:
+        username = None
+
     data = {
-        "access_token": request.session.get("access_token"),
+        "username": username,
     }
     return render(request, "profile.html", data)
