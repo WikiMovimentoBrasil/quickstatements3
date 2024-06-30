3b8cddb1112470cae83e10e56917e413fec16bde
Miguel Galves <mgalves@qipu.com.br>
2024-06-30 17:34:12 -0300

    Basic models ADMIN

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.utils.translation import gettext as _
+
+from core.models import Batch
+from core.models import BatchCommand
+
+
+@admin.register(Batch)
+class BatchAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user', 'status', 'created', 'modified']
+    search_field = ['name', 'user']
+    list_filter = ['status', 'created', 'modified']
+
+
+@admin.register(BatchCommand)
+class BatchCommandAdmin(admin.ModelAdmin):
+    list_select_related = ["batch",]
+    list_display = ['batch', 'index', 'status', 'created', 'modified']
+    list_filter = ['status', 'created', 'modified']
+    search_field = ['batch__name', 'batch__user']
+    raw_id_field = ['batch']
+
+
