63655a1a9f5c5087449aac8d60ae97aa271b7b6f
Miguel Galves <mgalves@qipu.com.br>
2024-06-30 18:14:45 -0300

    Add web to INSTALLED_APPS

--- src/qsts3/settings.py
+++ src/qsts3/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'web'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,4 @@ STATIC_ROOT = os.getenv('DJANGO_STATIC_ROOT')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
