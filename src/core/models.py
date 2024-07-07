f119da4cd0229b4a7f746e01f389657d87b87b6b
Miguel Galves <mgalves@qipu.com.br>
2024-07-07 18:10:57 -0300

    New parser

--- src/core/models.py
+++ src/core/models.py
@@ -8,11 +8,12 @@ class Batch(models.Model):
     """
     Represents a BATCH, containing multiple commands
     """
+
     class STATUS(Enum):
-        BLOCKED = (-1, _('Blocked'))
-        INITIAL = (0, _('Initial'))
-        RUNNING = (1, _('Running'))
-        DONE = (2, _('Done'))
+        BLOCKED = (-1, _("Blocked"))
+        INITIAL = (0, _("Initial"))
+        RUNNING = (1, _("Running"))
+        DONE = (2, _("Done"))
 
     name = models.CharField(max_length=255, blank=False, null=False)
     user = models.CharField(max_length=128, blank=False, null=False, db_index=True)
@@ -32,21 +33,75 @@ class Batch(models.Model):
         return BatchCommand.objects.filter(batch=self).all().order_by("index")
 
 
+class BatchCommandManager(models.Manager):
+    def create_command_from_v1(self, batch, index, raw_command):
+        status = BatchCommand.STATUS_INITIAL
+        message = None
+        command = {}
+
+        # comment = ''
+        # if ( re.find ( '/^(.*?) *\/\* *(.*?) *\*\/ *$/' , $row , $m ) ) { // Extract comment as summary
+        #     comment = $m[2] ;
+        #     raw_command = $m[1] ;
+        # }
+        elements = raw_command.split("\t")
+        if len(elements) == 0:
+            return None
+
+        llen = len(elements)
+        first_command = elements[0].upper().strip()
+
+        if first_command == "CREATE":
+            if llen != 1:
+                message = "CREATE command can have only 1 column"
+                status = BatchCommand.STATUS_ERROR
+            else:
+                command.update({"action": "create", "type": "item"})
+
+        elif first_command == "MERGE":
+            if llen != 3:
+                message = "MERGE command must have 3 columns"
+                status = BatchCommand.STATUS_ERROR
+            else:
+                item1 = elements[1].strip()
+                item2 = elements[2].strip()
+                try:
+                    item1_id = int(item1[1:])
+                    item2_id = int(item2[1:])
+                    if item1_id > item2_id:
+                        # Always merge into older item
+                        item1, item2 = item2, item1
+                    command.update({"action": "merge", "type": "item", "item1": item1, "item2": item2})
+                except ValueError:
+                    message = f"MERGE items wrong format item1=[{item1}] item2=[{item2}]"
+                    status = BatchCommand.STATUS_ERROR
+
+        return self.create(batch=batch, index=index, json=command, raw=raw_command, status=status, message=message)
+
+    def create_command_from_csv(self, batch, index, raw_command):
+        return self.create(batch=batch, index=index, json=command, raw=raw_command)
+
+
 class BatchCommand(models.Model):
     """
     Individual command from a batch
     """
-    class STATUS(Enum):
-        BLOCKED = (-1, _('Error'))
-        INITIAL = (0, _('Initial'))
-        RUNNING = (1, _('Running'))
-        DONE = (2, _('Done'))
+
+    STATUS_ERROR = -1
+    STATUS_INITIAL = 0
+    STATUS_RUNNING = 1
+    STATUS_DONE = 2
+
+    STATUS_CHOICES = ((-1, _("Error")), (0, _("Initial")), (1, _("Running")), (2, _("Done")))
+
+    objects = BatchCommandManager()
 
     batch = models.ForeignKey(Batch, null=False, on_delete=models.CASCADE)
     index = models.IntegerField()
     json = models.JSONField()
-    status = models.IntegerField(default=STATUS.INITIAL, choices=[s.value for s in STATUS], null=False, db_index=True)
-    message = models.TextField()
+    status = models.IntegerField(default=STATUS_INITIAL, choices=STATUS_CHOICES, null=False, db_index=True)
+    raw = models.TextField()
+    message = models.TextField(blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
--- src/core/tests.py
+++ src/core/tests.py
@@ -1,3 +1,81 @@
 from django.test import TestCase
 
-# Create your tests here.
+from core.models import Batch
+from core.models import BatchCommand
+
+
+class TestBatchCommand(TestCase):
+    
+    def setUp(self):
+        self.batch = Batch.objects.create(name="Batch", user="wikiuser")
+
+    def test_v1_correct_create_command(self):
+        command = BatchCommand.objects.create_command_from_v1(self.batch, 0, "CREATE")
+        self.assertEqual(command.batch, self.batch)
+        self.assertEqual(command.index, 0)
+        self.assertEqual(command.json, {'action': 'create' , 'type': 'item'})
+        self.assertEqual(command.status, BatchCommand.STATUS_INITIAL)
+        command = BatchCommand.objects.create_command_from_v1(self.batch, 0, "CREATE ")
+        self.assertEqual(command.batch, self.batch)
+        self.assertEqual(command.index, 0)
+        self.assertEqual(command.json, {'action': 'create' , 'type': 'item'})
+        self.assertEqual(command.status, BatchCommand.STATUS_INITIAL)
+        command = BatchCommand.objects.create_command_from_v1(self.batch, 0, " CREATE ")
+        self.assertEqual(command.batch, self.batch)
+        self.assertEqual(command.index, 0)
+        self.assertEqual(command.json, {'action': 'create' , 'type': 'item'})
+        self.assertEqual(command.status, BatchCommand.STATUS_INITIAL)
+
+    def test_v1_bad_create_command(self):
+        command = BatchCommand.objects.create_command_from_v1(self.batch, 0, "CREATE\tQ123\t")
+        self.assertEqual(command.batch, self.batch)
+        self.assertEqual(command.index, 0)
+        self.assertEqual(command.json, {})
+        self.assertEqual(command.message, "CREATE command can have only 1 column")
+        self.assertEqual(command.status, BatchCommand.STATUS_ERROR)
+
+    def test_v1_correct_merge_command(self):
+        command = BatchCommand.objects.create_command_from_v1(self.batch, 0, "MERGE\tQ1\tQ2")
+        self.assertEqual(command.batch, self.batch)
+        self.assertEqual(command.index, 0)
+        self.assertEqual(command.json, {'action': 'merge' , 'type': 'item', 'item1': 'Q1', 'item2': 'Q2'})
+        self.assertEqual(command.status, BatchCommand.STATUS_INITIAL)
+        command = BatchCommand.objects.create_command_from_v1(self.batch, 0, "MERGE\tQ2\tQ1")
+        self.assertEqual(command.batch, self.batch)
+        self.assertEqual(command.index, 0)
+        self.assertEqual(command.json, {'action': 'merge' , 'type': 'item', 'item1': 'Q1', 'item2': 'Q2'})
+        self.assertEqual(command.status, BatchCommand.STATUS_INITIAL)
+        command = BatchCommand.objects.create_command_from_v1(self.batch, 0, "MERGE \tQ1 \tQ2 ")
+        self.assertEqual(command.batch, self.batch)
+        self.assertEqual(command.index, 0)
+        self.assertEqual(command.json, {'action': 'merge' , 'type': 'item', 'item1': 'Q1', 'item2': 'Q2'})
+        self.assertEqual(command.status, BatchCommand.STATUS_INITIAL)
+
+    def test_v1_bad_merge_command(self):
+        command = BatchCommand.objects.create_command_from_v1(self.batch, 0, "MERGE")
+        self.assertEqual(command.batch, self.batch)
+        self.assertEqual(command.index, 0)
+        self.assertEqual(command.json, {})
+        self.assertEqual(command.status, BatchCommand.STATUS_ERROR)
+        self.assertEqual(command.message, "MERGE command must have 3 columns")
+        command = BatchCommand.objects.create_command_from_v1(self.batch, 0, "MERGE\tQ1")
+        self.assertEqual(command.batch, self.batch)
+        self.assertEqual(command.index, 0)
+        self.assertEqual(command.json, {})
+        self.assertEqual(command.status, BatchCommand.STATUS_ERROR)
+        self.assertEqual(command.message, "MERGE command must have 3 columns")
+        command = BatchCommand.objects.create_command_from_v1(self.batch, 0, "MERGE\tQ1\t")
+        self.assertEqual(command.batch, self.batch)
+        self.assertEqual(command.index, 0)
+        self.assertEqual(command.json, {})
+        self.assertEqual(command.status, BatchCommand.STATUS_ERROR)
+        self.assertEqual(command.message, "MERGE items wrong format item1=[Q1] item2=[]")
+        command = BatchCommand.objects.create_command_from_v1(self.batch, 0, "MERGE\tQ1\tQ2\tQ3")
+        self.assertEqual(command.batch, self.batch)
+        self.assertEqual(command.index, 0)
+        self.assertEqual(command.json, {})
+        self.assertEqual(command.status, BatchCommand.STATUS_ERROR)
+        self.assertEqual(command.message, "MERGE command must have 3 columns")
+
+
+    
