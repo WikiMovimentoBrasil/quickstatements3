2470e56f98a16e6ae6778228b9f270387eb1e83a
arcstur <arcstur@gmail.com>
2024-08-05 17:32:38 -0300

    feat: create Token database model

--- src/web/models.py
+++ src/web/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext as _
 
-# Create your models here.
+class Token(models.Model):
+    """
+    User OAuth tokens
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=False, null=False)
+    # TODO: store this encrypted?
+    value = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Token for {self.user}: [redacted]"
+
+    class Meta:
+        verbose_name = _("Token")
+        verbose_name_plural = _("Tokens")
