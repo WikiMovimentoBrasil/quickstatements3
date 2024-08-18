cdb8481dfec16918ea81176b07b73abc371e4f2d
Miguel Galves <mgalves@qipu.com.br>
2024-08-18 16:44:14 -0300

    Adding new tests

--- src/web/tests/test_views.py
+++ src/web/tests/test_views.py
@@ -1,5 +1,5 @@
 
-
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test import Client
 from django.urls import reverse
@@ -62,6 +62,23 @@ class ViewsTest(TestCase):
         self.assertTemplateUsed("batch.html")
         self.assertEqual(response.context["batch"], batch)
 
+        response = c.get(f"/batch/{batch.pk}/summary/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("batch_summary.html")
+        self.assertEqual(response.context["pk"], batch.pk)
+        self.assertEqual(response.context["status"], "Initial")
+        self.assertEqual(response.context["error_count"], 0)
+        self.assertEqual(response.context["initial_count"], 0)
+        self.assertEqual(response.context["running_count"], 0)
+        self.assertEqual(response.context["done_count"], 0)
+        self.assertEqual(response.context["total_count"], 0)
+        self.assertEqual(response.context["done_percentage"], 0)
+
+        response = c.get(f"/batch/{batch.pk}/commands/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("batch_commands.html")
+        self.assertEqual(response.context["batch_pk"], batch.pk)
+
     def test_existing_batches(self):
         b1 = Batch.objects.create(name="My new batch", user="mgalves80")
         b2 = Batch.objects.create(name="My new batch", user="mgalves80")
@@ -85,5 +102,71 @@ class ViewsTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed("batches.html")
         self.assertEqual(list(response.context["page"].object_list), [b3, b2, b1])
+
+    def test_create_v1_batch_logged_user(self):
+        c = Client()
+        user = User.objects.create_user(username="john")
+        c.force_login(user)
+
+        # Black box testing. We dont have any batch listed
+        response = c.get("/batches/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("batches.html")
+        self.assertEqual(list(response.context["page"].object_list), [])
+
+        # Creating our new batch
+        response = c.get("/batch/new/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("new_batch.html")
+
+        response = c.post("/batch/new/", data={"name": "My v1 batch", "type": "v1", "commands": "CREATE||-Q1234|P1|12||Q222|P4|9~0.1"})
+        self.assertEqual(response.status_code, 302)
+
+        # Lets view the new batch
+        response = c.get(response.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("batch.html")
+        batch = response.context["batch"]
+        self.assertEqual(batch.name, "My v1 batch")
+        self.assertEqual(batch.batchcommand_set.count(), 3)
+
+        # Listing again. Now we have something
+        response = c.get("/batches/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("batches.html")
+        self.assertEqual(list(response.context["page"].object_list), [batch])
+
+    def test_create_csv_batch_logged_user(self):
+        c = Client()
+        user = User.objects.create_user(username="john")
+        c.force_login(user)
+
+        # Black box testing. We dont have any batch listed
+        response = c.get("/batches/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("batches.html")
+        self.assertEqual(list(response.context["page"].object_list), [])
+
+        # Creating our new batch
+        response = c.get("/batch/new/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("new_batch.html")
+
+        response = c.post("/batch/new/", data={"name": "My CSV batch", "type": "csv", "commands": "qid,P31,-P31"})
+        self.assertEqual(response.status_code, 302)
+
+        # Lets view the new batch
+        response = c.get(response.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("batch.html")
+        batch = response.context["batch"]
+        self.assertEqual(batch.name, "My CSV batch")
+        self.assertEqual(batch.batchcommand_set.count(), 0)
+
+        # Listing again. Now we have something
+        response = c.get("/batches/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("batches.html")
+        self.assertEqual(list(response.context["page"].object_list), [batch])
 
 
