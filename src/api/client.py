0de63bf41216844de445f41451219f2dad9a81f5
arcstur <arcstur@gmail.com>
2024-08-19 11:23:40 -0300

    feat(client): create method to POST do Wikidata REST API

--- src/api/client.py
+++ src/api/client.py
@@ -4,6 +4,7 @@ import requests
 class Client:
     BASE_URL = "https://www.mediawiki.org/w/rest.php/"
     ENDPOINT_PROFILE = f"{BASE_URL}oauth2/resource/profile"
+    WIKIDATA_URL = "https://www.wikidata.org/w/rest.php/wikibase/v0"
 
     def __init__(self, token):
         self.token = token
@@ -20,7 +21,8 @@ class Client:
 
     def request_get(self, endpoint):
         return requests.get(
-            endpoint, headers=self.headers()
+            endpoint,
+            headers=self.headers(),
         ).json()
 
     def get_username(self):
@@ -33,3 +35,17 @@ class Client:
                 "The token did not return a valid username.",
                 response,
             )
+
+    def wikidata_post(self, endpoint, body):
+        print(f"Sending request at {endpoint} with body {body}")
+        res = requests.post(
+            f"{self.WIKIDATA_URL}{endpoint}",
+            headers=self.headers(),
+            json=body,
+        )
+        print(f"Response content: {res.json()}")
+        res.raise_for_status()
+
+    def wikidata_statement_post(self, item_id, body):
+        endpoint = f"/entities/items/{item_id}/statements"
+        return self.wikidata_post(endpoint, body)
