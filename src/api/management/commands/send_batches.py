32184e64c304621f7d460f2c0adedd817fcd5407
arcstur <arcstur@gmail.com>
2024-09-02 13:50:20 -0300

    feat: use uwsgi to spawn background process to send batches

--- src/api/management/commands/send_batches.py
+++ src/api/management/commands/send_batches.py
@@ -1,43 +1,32 @@
+import time
+
 from django.core.management.base import BaseCommand
 from core.models import Batch
 
 
 class Command(BaseCommand):
+    TIMEOUT_SEC = 10
+
     help = "Sends all available batches to the Wikidata API"
 
     def handle(self, *args, **options):
-        while True:
-            batch = self._get_first_batch()
-            if batch is None:
-                self._exit_msg()
-                break
-            else:
-                self._run_batch(batch)
-                self._print_result(batch)
+        self.send_start_message()
 
-    def _get_first_batch(self):
+        while True:
+            batch = self.get_first_batch()
+            if batch:
+                batch.run()
+            else:
+                time.sleep(self.TIMEOUT_SEC)
+
+    def send_start_message(self):
+        msg = self.style.SUCCESS("==> send_batches management command started!")
+        self.stdout.write(msg)
+
+    def get_first_batch(self):
         return (
             Batch.objects.select_for_update()
             .filter(status=Batch.STATUS_INITIAL)
             .order_by("id")
             .first()
         )
-
-    def _exit_msg(self):
-        msg = self.style.SUCCESS("==> No more batches to run.")
-        self.stdout.write(msg)
-
-    def _run_batch(self, batch):
-        self.stdout.write(f"==> Running batch: {batch}")
-        batch.run()
-
-    def _print_result(self, batch):
-        if batch.status == Batch.STATUS_DONE:
-            msg = self.style.SUCCESS(f"====> Batch ran successfully: {batch}")
-        elif batch.status == Batch.STATUS_BLOCKED:
-            msg = self.style.ERROR(f"====> Batch was blocked: {batch}")
-        else:
-            msg = self.style.WARNING(f"====> Batch was not finished: {batch}")
-
-        self.stdout.write(msg)
-        self.stdout.write("")
