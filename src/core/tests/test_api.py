59d125cca34ed3c394137333fff5482d6526835f
arcstur <arcstur@gmail.com>
2024-10-02 16:23:45 -0300

    tests: create test using all available data types

--- src/core/tests/test_api.py
+++ src/core/tests/test_api.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 from core.client import Client
 from core.exceptions import NonexistantPropertyOrNoDataType
 from core.exceptions import NoValueTypeForThisDataType
+from core.exceptions import InvalidPropertyValueType
 
 
 class ApiMocker:
@@ -41,6 +42,14 @@ class ApiMocker:
         mocker.get(
             client.wikibase_entity_url(entity_id, "/labels"),
             json=labels,
+            status_code=200,
+        )
+
+    @classmethod
+    def create_item(cls, mocker, item_id):
+        mocker.post(
+            cls.wikibase_url("/entities/items"),
+            json={"id": item_id},
             status_code=200,
         )
 
@@ -101,3 +110,67 @@ class ClientTests(TestCase):
         ApiMocker.labels(mocker, client, entity_id, labels)
         returned_labels = client.get_labels(entity_id)
         self.assertEqual(labels, returned_labels)
+
+    @requests_mock.Mocker()
+    def test_verify_value_type(self, mocker):
+        ApiMocker.property_data_type(mocker, "P1", "commonsMedia")
+        ApiMocker.property_data_type(mocker, "P2", "geo-shape")
+        ApiMocker.property_data_type(mocker, "P3", "tabular-data")
+        ApiMocker.property_data_type(mocker, "P4", "url")
+        ApiMocker.property_data_type(mocker, "P5", "external-id")
+        ApiMocker.property_data_type(mocker, "P6", "wikibase-item")
+        ApiMocker.property_data_type(mocker, "P7", "wikibase-property")
+        ApiMocker.property_data_type(mocker, "P8", "globe-coordinate")
+        ApiMocker.property_data_type(mocker, "P9", "monolingualtext")
+        ApiMocker.property_data_type(mocker, "P10", "quantity")
+        ApiMocker.property_data_type(mocker, "P11", "string")
+        ApiMocker.property_data_type(mocker, "P12", "time")
+        ApiMocker.property_data_type(mocker, "P13", "musical-notation")
+        ApiMocker.property_data_type(mocker, "P14", "math")
+        ApiMocker.property_data_type(mocker, "P15", "wikibase-lexeme")
+        ApiMocker.property_data_type(mocker, "P16", "wikibase-form")
+        ApiMocker.property_data_type(mocker, "P17", "wikibase-sense")
+        ApiMocker.property_data_type(mocker, "P18", "entity-schema")
+
+        client = self.api_client()
+
+        all_value_types = (
+            "globecoordinate",
+            "monolingualtext",
+            "quantity",
+            "string",
+            "time",
+            "wikibase-entityid",
+        )
+
+        correct_value_types = {
+            "P1": "string",
+            "P2": "string",
+            "P3": "string",
+            "P4": "string",
+            "P5": "string",
+            "P6": "wikibase-entityid",
+            "P7": "wikibase-entityid",
+            "P8": "globecoordinate",
+            "P9": "monolingualtext",
+            "P10": "quantity",
+            "P11": "string",
+            "P12": "time",
+            "P13": "string",
+            "P14": "string",
+            "P15": "wikibase-entityid",
+            "P16": "wikibase-entityid",
+            "P17": "wikibase-entityid",
+            "P18": "wikibase-entityid",
+        }
+
+        for i in range(18):
+            property_id = f"P{i + 1}"
+            client.verify_value_type(property_id, correct_value_types[property_id])
+
+        for i in range(18):
+            property_id = f"P{i + 1}"
+            for v in all_value_types:
+                if v != correct_value_types[property_id]:
+                    with self.assertRaises(InvalidPropertyValueType):
+                        client.verify_value_type(property_id, v)
--- src/core/tests/test_batch_processing.py
+++ src/core/tests/test_batch_processing.py
@@ -72,3 +72,69 @@ class ProcessingTests(TestCase):
         self.assertEqual(commands[7].status, BatchCommand.STATUS_ERROR)
         self.assertEqual(commands[8].status, BatchCommand.STATUS_INITIAL)
         self.assertEqual(len(commands), 9)
+
+    @requests_mock.Mocker()
+    def test_all_data_types(self, mocker):
+        ApiMocker.create_item(mocker, "Q123")
+        ApiMocker.add_statement_successful(mocker, "Q123")
+        ApiMocker.property_data_type(mocker, "P1", "commonsMedia")
+        ApiMocker.property_data_type(mocker, "P2", "geo-shape")
+        ApiMocker.property_data_type(mocker, "P3", "tabular-data")
+        ApiMocker.property_data_type(mocker, "P4", "url")
+        ApiMocker.property_data_type(mocker, "P5", "external-id")
+        ApiMocker.property_data_type(mocker, "P6", "wikibase-item")
+        ApiMocker.property_data_type(mocker, "P7", "wikibase-property")
+        ApiMocker.property_data_type(mocker, "P8", "globe-coordinate")
+        ApiMocker.property_data_type(mocker, "P9", "monolingualtext")
+        ApiMocker.property_data_type(mocker, "P10", "quantity")
+        ApiMocker.property_data_type(mocker, "P11", "string")
+        ApiMocker.property_data_type(mocker, "P12", "time")
+        ApiMocker.property_data_type(mocker, "P13", "musical-notation")
+        ApiMocker.property_data_type(mocker, "P14", "math")
+        ApiMocker.property_data_type(mocker, "P15", "wikibase-lexeme")
+        ApiMocker.property_data_type(mocker, "P16", "wikibase-form")
+        ApiMocker.property_data_type(mocker, "P17", "wikibase-sense")
+        ApiMocker.property_data_type(mocker, "P18", "entity-schema")
+
+        raw = """CREATE
+        LAST|P1|"MyCommonsMedia.jpg"
+        LAST|P2|"@43.26193/10.92708"
+        LAST|P3|"my,tabular,data"
+        LAST|P4|"https://www.myurl.com"
+        LAST|P5|"123.456.789-00"
+        LAST|P6|Q123
+        LAST|P7|P123
+        LAST|P8|@43.26193/10.92708
+        LAST|P9|pt:"monolingualtext"
+        LAST|P10|12345678
+        LAST|P11|"string"
+        LAST|P12|+1967-01-17T00:00:00Z/11
+        LAST|P13|"my musical notation"
+        LAST|P14|"my mathematical notation"
+        LAST|P15|L123
+        LAST|P16|L123-F123
+        LAST|P17|L123-S123
+        LAST|P18|Q123"""
+        batch = self.parse_and_run(raw)
+        self.assertEqual(batch.status, Batch.STATUS_DONE)
+
+        commands = batch.commands()
+        self.assertEqual(commands[0].status, BatchCommand.STATUS_DONE)
+        self.assertEqual(commands[1].status, BatchCommand.STATUS_DONE)
+        self.assertEqual(commands[2].status, BatchCommand.STATUS_DONE)
+        self.assertEqual(commands[3].status, BatchCommand.STATUS_DONE)
+        self.assertEqual(commands[4].status, BatchCommand.STATUS_DONE)
+        self.assertEqual(commands[5].status, BatchCommand.STATUS_DONE)
+        self.assertEqual(commands[6].status, BatchCommand.STATUS_DONE)
+        self.assertEqual(commands[7].status, BatchCommand.STATUS_DONE)
+        self.assertEqual(commands[8].status, BatchCommand.STATUS_DONE)
+        self.assertEqual(commands[9].status, BatchCommand.STATUS_DONE)
+        self.assertEqual(commands[10].status, BatchCommand.STATUS_DONE)
+        self.assertEqual(commands[11].status, BatchCommand.STATUS_DONE)
+        self.assertEqual(commands[12].status, BatchCommand.STATUS_DONE)
+        self.assertEqual(commands[13].status, BatchCommand.STATUS_DONE)
+        self.assertEqual(commands[14].status, BatchCommand.STATUS_DONE)
+        self.assertEqual(commands[15].status, BatchCommand.STATUS_DONE)
+        self.assertEqual(commands[16].status, BatchCommand.STATUS_DONE)
+        self.assertEqual(commands[17].status, BatchCommand.STATUS_DONE)
+        self.assertEqual(commands[18].status, BatchCommand.STATUS_DONE)
