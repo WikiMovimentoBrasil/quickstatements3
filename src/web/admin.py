04a4500ed1dca5de9dd6a78b16a62678447bb493
arcstur <arcstur@gmail.com>
2024-11-08 15:07:24 -0300

    feat: add web app to admin and improve core admin

--- src/web/admin.py
+++ src/web/admin.py
@@ -1,2 +1,20 @@
+from django.contrib import admin
 
-# Register your models here.
+from .models import Token
+from .models import Preferences
+
+
+@admin.register(Token)
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ["user", "expires_at"]
+    list_filter = ["expires_at"]
+    search_field = ["user"]
+    raw_id_fields = ["user"]
+
+
+@admin.register(Preferences)
+class PreferencesAdmin(admin.ModelAdmin):
+    list_display = ["user", "language"]
+    list_filter = ["language"]
+    search_field = ["user"]
+    raw_id_fields = ["user"]
