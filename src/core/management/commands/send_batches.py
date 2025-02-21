0b29b44149d03ad2c8594db968c55bb152aa0267
Raphael Lullis <raphael@mushroomlabs.com>
2025-02-21 15:59:45 +0100

    Add a 10 second sleep time at the end of each run

--- src/core/management/commands/send_batches.py
+++ src/core/management/commands/send_batches.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 from core.models import Batch
 from django.core.management.base import BaseCommand
@@ -8,17 +9,21 @@ logger = logging.getLogger("qsts3")
 
 
 class Command(BaseCommand):
+    TIMEOUT_SEC = 10
     help = "Sends all available batches to the Wikidata API"
 
     def handle(self, *args, **options):
         logger.info("[command] send_batches management command started!")
-        batches = Batch.objects.filter(status=Batch.STATUS_INITIAL)
+        pending_batches = Batch.objects.filter(status=Batch.STATUS_INITIAL)
 
-        for batch in (
-            batches.select_for_update(skip_locked=True).order_by("?").iterator()
-        ):
-            with transaction.atomic():
-                try:
-                    batch.run()
-                except Exception as exc:
-                    logger.exception(f"Failed to process {batch}: {exc}")
+        while True:
+            batches = pending_batches.select_for_update(skip_locked=True)
+            for batch in batches.order_by("?").iterator():
+                with transaction.atomic():
+                    try:
+                        batch.run()
+                    except Exception as exc:
+                        logger.exception(f"Failed to process {batch}: {exc}")
+            else:
+                logger.info(f"No batches to process. Sleeping {self.TIMEOUT_SEC}s...")
+                time.sleep(self.TIMEOUT_SEC)
