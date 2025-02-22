7f6d54ce9f7095a5f4eabb82732baf8e391f7cf3
Carybé <carybe.silva@usp.br>
2025-02-21 23:28:13 -0300

    chore: re-enable Toolforge deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,21 @@
 
 set -eu
 
-# cd ~/www/python || exit
+cd ~/www/python || exit
 
-# echo "==> Updating git repository..."
-# git pull
+echo "==> Updating git repository..."
+git pull
 
-# echo "==> Entering python shell..."
-# toolforge webservice --backend=kubernetes python3.11 shell -- \
-#   webservice-python-bootstrap && \
-#   source venv/bin/activate && \
-#   echo "==> Installing dependencies..." && \
-#   pip install -r requirements.txt && \
-#   echo "==> Running migrations..." && \
-#   python3 src/manage.py migrate && \
-#   echo "==> Collecting static files..." && \
-#   python3 src/manage.py collectstatic --noinput
+echo "==> Entering python shell..."
+toolforge webservice --backend=kubernetes python3.11 shell -- \
+  webservice-python-bootstrap && \
+  source venv/bin/activate && \
+  echo "==> Installing dependencies..." && \
+  pip install -r requirements.txt && \
+  echo "==> Running migrations..." && \
+  python3 src/manage.py migrate && \
+  echo "==> Collecting static files..." && \
+  python3 src/manage.py collectstatic --noinput
 
-# echo "==> Restarting webservice..."
-# toolforge webservice --backend=kubernetes python3.11 restart
+echo "==> Restarting webservice..."
+toolforge webservice --backend=kubernetes python3.11 restart
