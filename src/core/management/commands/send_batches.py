65e9397d35e2d2deb64860ecccc7cd93571692b1
Raphael Lullis <raphael@mushroomlabs.com>
2025-03-06 15:10:51 +0100

    Make send_batches run in separate threads, one per user

--- src/core/management/commands/send_batches.py
+++ src/core/management/commands/send_batches.py
@@ -1,4 +1,5 @@
 import logging
+import threading
 import time
 
 from core.models import Batch
@@ -8,22 +9,44 @@ from django.db import transaction
 logger = logging.getLogger("qsts3")
 
 
+def process_batches(batches):
+    for batch in batches.iterator():
+        with transaction.atomic():
+            try:
+                batch.run()
+            except Exception as exc:
+                logger.exception(f"Failed to process {batch}: {exc}")
+
+
 class Command(BaseCommand):
-    TIMEOUT_SEC = 10
+    TIMEOUT_SEC = 2
     help = "Sends all available batches to the Wikidata API"
 
     def handle(self, *args, **options):
         logger.info("[command] send_batches management command started!")
-        pending_batches = Batch.objects.filter(status=Batch.STATUS_INITIAL)
+        user_threads = {}
 
         while True:
-            batches = pending_batches.select_for_update(skip_locked=True)
-            for batch in batches.order_by("?").iterator():
-                with transaction.atomic():
-                    try:
-                        batch.run()
-                    except Exception as exc:
-                        logger.exception(f"Failed to process {batch}: {exc}")
-            else:
-                logger.info(f"No batches to process. Sleeping {self.TIMEOUT_SEC}s...")
-                time.sleep(self.TIMEOUT_SEC)
+            batches = Batch.objects.filter(status=Batch.STATUS_INITIAL)
+            users = batches.values_list("user", flat=True).distinct()
+
+            completed = [u for u, t in user_threads.items() if not t.is_alive()]
+            for user in completed:
+                del user_threads[user]
+
+            for user in users:
+                thread = user_threads.get(user, None)
+                if thread is not None and thread.is_alive():
+                    logger.info(f"Thread for user {user} is running...")
+                    continue
+
+                user_batches = batches.filter(user=user)
+                thread = threading.Thread(
+                    target=process_batches, daemon=True, args=(user_batches,)
+                )
+                logger.info(f"Starting thread for user {user}...")
+                thread.start()
+                user_threads[user] = thread
+
+            logger.info(f"No batches to process. Sleeping {self.TIMEOUT_SEC}s...")
+            time.sleep(self.TIMEOUT_SEC)
