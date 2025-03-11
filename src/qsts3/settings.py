71bd0776e5f9213cf59a09c432a30e168ee8da9a
Carybé <carybe.silva@usp.br>
2025-03-11 11:42:06 -0300

    Rollback to prevous debug variable setup

--- src/qsts3/settings.py
+++ src/qsts3/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("QSTS_DEBUG", True)
+DEBUG = "QSTS_DEBUG" in os.environ
 
 ALLOWED_HOSTS = ["qs-dev.toolforge.org", "localhost"]
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8000", "https://qs-dev.toolforge.org/"]
