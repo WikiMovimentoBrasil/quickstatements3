340d36b8d1b12beadf8c702b0ad2ae9f424b08e6
Carybé <carybe.silva@usp.br>
2025-03-13 19:34:35 -0300

    Refactor deployment script to use kubectl exec instead of the toolforge webservice shell commands

--- deploy.sh
+++ deploy.sh
@@ -4,29 +4,37 @@ set -eu
 
 PROJECT_DIR="${HOME}/www/python"
 SRC_DIR="${PROJECT_DIR}/src"
+POD="$(kubectl get pods -n tool-qs-dev -o name| head -n1 | cut -d/ -f2)"
 
-cd "${PROJECT_DIR}" || exit
+cd "${PROJECT_DIR}" || exit 1
 
 echo "==> Updating git repository..."
 git pull
 
 echo "==> Entering python shell..."
-toolforge webservice --backend=kubernetes python3.11 shell -- \
-  echo "==> Installing dependencies..." && \
-  export MYSQLCLIENT_CFLAGS="-I/usr/include/mariadb/" && \
-  export MYSQLCLIENT_LDFLAGS="-L/usr/lib/x86_64-linux-gnu/ -lmariadb" && \
+kubectl exec "${POD}" -- bash -euc \
+"
+  echo '==> Installing dependencies...' && \
+  export MYSQLCLIENT_CFLAGS='-I/usr/include/mariadb/' && \
+  export MYSQLCLIENT_LDFLAGS='-L/usr/lib/x86_64-linux-gnu/ -lmariadb' && \
   webservice-python-bootstrap && \
-  source "${PROJECT_DIR}/venv/bin/activate" && \
-  echo "==> Running migrations..." && \
-  python3 "${SRC_DIR}/manage.py" migrate && \
-  echo "==> Collecting static files..." && \
-  python3 "${SRC_DIR}/manage.py" collectstatic --noinput
+  source \"${PROJECT_DIR}/venv/bin/activate\" && \
+  echo '==> Running migrations...' && \
+  python3 \"${SRC_DIR}/manage.py\" migrate && \
+  echo '==> Collecting static files...' && \
+  python3 \"${SRC_DIR}/manage.py\" collectstatic --noinput
+"
 
-echo "==> Restarting webservice..."
-toolforge webservice --backend=kubernetes --mem 6Gi python3.11 restart
+echo '==> Restarting webservice...'
+toolforge webservice --backend=kubernetes --mem 4Gi python3.11 restart
 
-echo "==> Restarting batches..."
-toolforge webservice --backend=kubernetes python3.11 shell -- \
-  webservice-python-bootstrap  && \
-  source "${PROJECT_DIR}/venv/bin/activate" && \
-  python3 "${SRC_DIR}/manage.py" restart_batches
+echo '==> Pausing briefly to allow the container to start...'
+sleep 10
+
+POD="$(kubectl get pods -n tool-qs-dev -o name| head -n1 | cut -d/ -f2)"
+echo '==> Restarting batches...'
+kubectl exec "${POD}" -- bash -euc \
+"
+  source \"${PROJECT_DIR}/venv/bin/activate\" && \
+  python3 \"${SRC_DIR}/manage.py\" restart_batches
+"
